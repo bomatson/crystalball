fc91c1390c079a92dda2285b0468a47ecd8904c7
bomatson <bobby.matson@gmail.com>
2011-12-05 22:24:14 -0800

    Scaffold questions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crystalball::Application.config.secret_token = 'dbd20a7755e15510d441a1e56e7a68dd1eade11653487b1f97eaffbf85f22e634e0464cfb8991201ac760fdf6ad392a886826e025d83ad07df9da4085f9523ab'
